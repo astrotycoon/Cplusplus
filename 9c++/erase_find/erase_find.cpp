bc99092b7ba0c813588d2cf2afaf9e3f0f56f8d9
astrotycoon <astrotycoon@yahoo.cn>
2012-09-18 21:52:45 +0800

    add

--- 9c++/erase_find/erase_find.cpp
+++ 9c++/erase_find/erase_find.cpp
@@ -7,6 +7,10 @@
 	using std::string;
 #include <algorithm>
 	using std::find;
+#include <list>
+	using std::list;
+#include <deque>
+	using std::deque;
 
 /////////////////////////////////////////////////////////////////////////////////
 template<typename T1>
@@ -24,14 +28,32 @@ template<typename T1>
 	}
 }
 /////////////////////////////////////////////////////////////////////////////////
-
+template<typename T1, typename T2>
+void erase_value(T1 &container, const T2 value)
+{
+	T1::iterator beg = container.begin();
+	while (beg != container.end())
+	{
+		T1::iterator iter_find = find(beg, container.end(), value);
+		if (iter_find != container.end())
+		{
+			beg = container.erase(iter_find);
+		}
+		else
+			++beg;
+	}
+}
+/////////////////////////////////////////////////////////////////////////////////
 int main(int argc, const char *argv[])
 {
 	string str[] = {"love", "hello", "world", "vim", "hello", "tom", "hello", "hello", "hello", "astrol", "tome", "jmp"};
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	cout << "\tvector: " << endl;
 	vector<string> istr(str, str + sizeof(str) / sizeof(string));
 	show(istr.begin(), istr.end());
 
-	std::vector<string>::iterator iter = istr.begin();
+//	std::vector<string>::iterator iter = istr.begin();
+#if 0
 	while (iter != istr.end())
 	{
 		std::vector<string>::iterator iter_find = find(iter, istr.end(), "hello");
@@ -40,8 +62,47 @@ int main(int argc, const char *argv[])
 		else
 			++iter;
 	}
-	
+#endif
+	erase_value(istr, "hello");
 	show(istr.begin(), istr.end());
-	
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	cout << "\tlist: " << endl;
+	list<string> slist(str, str + sizeof(str) / sizeof(string));
+	show(slist.begin(), slist.end());
+#if 0
+	for (list<string>::iterator iter_list = slist.begin(); iter_list != slist.end();)
+	{
+		if (*iter_list == "hello")
+		{
+			iter_list = slist.erase(iter_list);
+		}
+		else
+			++iter_list;
+	}
+#endif
+	erase_value(slist, "hello");
+	show(slist.begin(), slist.end());
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	cout << "\tdeque: " << endl;
+	deque<string> sdeque(str, str + sizeof(str) / sizeof(string));
+	show(sdeque.begin(), sdeque.end());
+
+//	deque<string>::iterator iter_deque = sdeque.begin();
+#if 0
+	while (iter_deque != sdeque.end())
+	{
+		deque<string>::iterator iter_find = find(iter_deque, sdeque.end(), "hello");
+		if (iter_find != sdeque.end())
+		{
+			iter_deque = sdeque.erase(iter_find);
+		}
+		else
+			++iter_deque;
+	}
+#endif
+	erase_value(sdeque, "hello");
+	show(sdeque.begin(), sdeque.end());
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	return 0;
 }
+
